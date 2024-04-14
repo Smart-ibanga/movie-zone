b965fb5e64417b54b92e59eaee0b5363614fee0a
yourname <ibangasmart@gmail.com>
2024-04-14 07:38:58 +0100

    fetch json data

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './banner.css'
 
 const Banner = () => {
+    const [movies, setMovies] = useState ([]);
+
+        const fetchData = () => {
+
+            fetch('http://localhost:3000/data/movieData.json')
+            .then(res => res.json())
+            .then(data => setMovies(data))
+            .catch(e => console.log(e.message));
+        };
+
+        useEffect ( () =>{
+            fetchData();
+        }, []);
+    
   return (
     <div className="banner">
-        
+
     </div>
   )
 }
