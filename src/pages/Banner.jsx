44a4faeacef7074c8801ba5b47c6afef5f1dd866
Smart-ibanga <ibangasmart@gmail.com>
2024-05-06 02:30:21 +0100

    continue development

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -23,7 +23,21 @@ const Banner = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-lg-6 col-md-12">
-              <div className="content"></div>
+              <div className="content">
+                <img src="" alt="Movie Title" className="movie-title" />
+                <h4><span>Year</span>
+                    <span>
+                      <i>age</i>
+                    </span>
+                    <span>length</span>
+                    <span>category</span>
+                </h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Commodi provident magni aliquid aspernatur quia modi tempora 
+                  sapiente ipsa rerum! Nobis magnam deleniti fugit ratione, 
+                  impedit quasi quod placeat quas? Repudiandae.
+                </p>
+              </div>
             </div>
            <div className="col-lg-6 col-md-12">
 
